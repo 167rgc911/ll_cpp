dbdd44df08f518c06b8bc70c19c5fdc20b9e8865
rgc <74342662+167rgc911@users.noreply.github.com>
2024-03-08 23:49:48 +0900

    overhaul

--- t/include/aa.h
+++ t/include/aa.h
@@ -22,23 +22,47 @@
 
 #include <map>
 #include <string>
+#include <iostream>
 
 typedef std::map<std::string, std::string> aa;
 
 class testAA
 {
     aa aa_;
-    std::string p0_;
-    std::string p1_;
 public:
+    const std::string& param0;
+    const std::string& param1;
+
     testAA(const std::string& p0, const std::string& p1)
-        : p0_(p0), p1_(p1)
+        :
+            param0(p0),
+            param1(p1)
     {
-        aa_.insert( {"p0", p0} );
-        aa_.insert( {"p1", p1} );
+        aa_.insert( {"param0", p0} );
+        aa_.insert( {"param1", p1} );
     };
-    ~testAA();
-    std::string& operator[](const std::string& p0);
+
+    const std::string& operator[](const std::string& p)
+    {
+        return aa_.at(p);
+        /* if (! aa_.empty()) */
+        /* { */
+        /*     auto s = aa_.find(p); */
+        /*     if (s != aa_.end()) */
+        /*     { */
+        /*         return s->second; */
+        /*     } */
+        /* } */
+        /* return {}; */
+    };
+
+    void print(const std::string comment)
+    {
+        std::cout << comment << "{";
+            for (const auto &pair : aa_)
+                std::cout << "{" << pair.first << ": " << pair.second << "}";
+            std::cout << "}\n";
+    };
 };
 
 #endif
